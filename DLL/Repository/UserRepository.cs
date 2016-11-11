d9f8e069dd6f3882b5df92a93a49db4f0e20cdf6
Raakost <jeanettesydboege@gmail.com>
2016-11-11 15:26:55 +0100

    User Repository added

--- DLL/Repository/UserRepository.cs
+++ DLL/Repository/UserRepository.cs
@@ -12,27 +12,58 @@ namespace DLL.Repository
     {
         public User Create(User t)
         {
-            throw new NotImplementedException();
+            using (var ctx = new FPContext())
+            {
+                ctx.Users.Add(t);
+                ctx.SaveChanges();
+                return t;
+            }
         }
 
         public bool Delete(User t)
         {
-            throw new NotImplementedException();
+            using (var ctx = new FPContext())
+            {
+                ctx.Entry(ctx.Users.FirstOrDefault(x => x.Id == t.Id)).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+                return ctx.Users.FirstOrDefault(x => x.Id == t.Id) == null;
+            }
         }
 
         public User Get(int id)
         {
-            throw new NotImplementedException();
+            using (var ctx = new FPContext())
+            {
+                return ctx.Users.FirstOrDefault(x => x.Id == id);
+
+            }
         }
 
         public List<User> ReadAll()
         {
-            throw new NotImplementedException();
+            using (var ctx = new FPContext())
+            {
+                return ctx.Users.ToList();
+            }
         }
 
         public User Update(User t)
         {
-            throw new NotImplementedException();
+            using (var ctx = new FPContext())
+            {
+                var userToUpdate = ctx.Users.FirstOrDefault(x => x.Id == t.Id);
+                if (userToUpdate != null)
+                {
+                    userToUpdate.FirstName = t.FirstName;
+                    userToUpdate.LastName = t.LastName;
+                    userToUpdate.Email = t.Email;
+                    userToUpdate.Company = t.Company;
+                    userToUpdate.Type = t.Type;
+                    userToUpdate.Password = t.Password;
+                }
+                ctx.SaveChanges();
+                return t;
+            }
         }
     }
 }
